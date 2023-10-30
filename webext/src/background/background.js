fe36b3e5bff43e00cacbd9a2f245e8a8b9b7dcff
Farseen <oss@itsfarseen.anonaddy.me>
2023-10-30 16:01:55 +0530

    Move source files from webext/ to webext/src
    This makes copying in build script easier.
    Anything you place in the src folder will be compiled or copied to the
    build folder.

---
+++ webext/src/background/background.js
@@ -0,0 +1,31 @@
+import browser from "webextension-polyfill"
+import * as api from "wallet-lib"
+
+const handleMessage = async (msg, _sender, cb) => {
+  console.log("Received %s", msg.type)
+
+  switch (msg.type) {
+
+    case 'privateKeyFromMnemonic':
+      const pk = await api.privateKeyFromMnemonic(msg.data)
+      return cb(pk)
+
+    case 'privateKeysToAddress':
+      const addr = await api.privateKeysToAddress(msg.data, null, 1) // No stake key passed in for now, 1 is mainnet id
+      return cb(addr)
+
+    case 'paymentKeyFromEnvelope':
+      const paymentKey = await api.paymentKeyFromEnvelope(msg.data)
+      return cb(paymentKey)
+
+    case 'stakeKeyFromEnvelope':
+      const stakeKey = await api.stakeKeyFromEnvelope(msg.data)
+      return cb(stakeKey)
+
+    default:
+      console.error('Unsupported msg type')
+      throw new Error('Unsupported msg type')
+  }
+}
+
+browser.runtime.onMessage.addListener(handleMessage)
