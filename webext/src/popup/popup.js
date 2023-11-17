38e3d18925366aefb765cd6428890ad911d12b7f
Farseen <oss@itsfarseen.anonaddy.me>
2023-11-17 12:05:49 +0530

    Implement the functionality using CSL instead of CTL

--- webext/src/popup/popup.js
+++ webext/src/popup/popup.js
@@ -1,33 +1,65 @@
-import browser from "webextension-polyfill";
+import * as bip39 from "bip39";
+import * as CardanoWasm from "@emurgo/cardano-serialization-lib-browser";
 
-console.log("popup")
-console.log(document.getElementById("btn-pk-from-mnemonic"))
-
-export const handlePkFromMnemonic = async () => {
-  const mnemonic = document.getElementById("pk-from-mnemonic")
-  const pk = await browser.runtime.sendMessage({ type: 'privateKeyFromMnemonic', data: mnemonic.value })
-  mnemonic.value = pk
+function harden(num) {
+  return 0x80000000 + num;
 }
 
-export const handlePkToAddress = async () => {
-  const pk = document.getElementById("pk-to-address")
-  const addr = await browser.runtime.sendMessage({ type: 'privateKeysToAddress', data: pk.value })
-  pk.value = addr
+function attachHandler(btnId, inputId, outputId, handler) {
+  const btn = document.getElementById(btnId);
+  const input = document.getElementById(inputId);
+  const output = document.getElementById(outputId);
+  btn.addEventListener(
+    "click",
+    () => (output.innerText = handler(input.value))
+  );
 }
 
-export const handlePaymentKeyFromEnveloe = async () => {
-  const envelope = document.getElementById("payment-key-from-envelope")
-  const paymentKey = await browser.runtime.sendMessage({ type: 'paymentKeyFromEnvelope', data: envelope.value })
-  envelope.value = paymentKey
+function pkFromMnemonic(mnemonic) {
+  const entropy = bip39.mnemonicToEntropy(mnemonic);
+  const rootKey = CardanoWasm.Bip32PrivateKey.from_bip39_entropy(
+    Buffer.from(entropy, "hex"),
+    Buffer.from("") // password
+  );
+  return rootKey.to_bech32();
 }
 
-export const stakePaymentKeyFromEnvelope = async () => {
-  const envelope = document.getElementById("stake-key-from-envelope")
-  const stakeKey = await browser.runtime.sendMessage({ type: 'stakeKeyFromEnvelope', data: envelope.value })
-  envelope.value = stakeKey
+function pkToAddresses(privateKey) {
+  const rootKey = CardanoWasm.Bip32PrivateKey.from_bech32(privateKey);
+  const accountKey = rootKey
+    .derive(harden(1852))
+    .derive(harden(1815))
+    .derive(harden(0));
+
+  const utxoPubkey = accountKey.derive(0).derive(0).to_public();
+  const stakeKey = accountKey.derive(2).derive(0).to_public();
+
+  const baseAddr = CardanoWasm.BaseAddress.new(
+    CardanoWasm.NetworkInfo.mainnet().network_id(),
+    CardanoWasm.StakeCredential.from_keyhash(utxoPubkey.to_raw_key().hash()),
+    CardanoWasm.StakeCredential.from_keyhash(stakeKey.to_raw_key().hash())
+  );
+
+  return [
+    `Payment Address:\n${utxoPubkey.to_bech32()}`,
+    `Stake Address:\n${stakeKey.to_bech32()}`,
+    `Base Address:\n${baseAddr.to_address().to_bech32()}`,
+  ].join("\n\n");
 }
 
-document.getElementById("btn-pk-from-mnemonic").addEventListener("click", handlePkFromMnemonic)
-document.getElementById("btn-pk-to-address").addEventListener("click", handlePkToAddress)
-document.getElementById("btn-payment-key-from-envelope").addEventListener("click", handlePaymentKeyFromEnveloe)
-document.getElementById("btn-stake-key-from-envelope").addEventListener("click", stakePaymentKeyFromEnvelope)
+attachHandler(
+  "btn-pk-from-mnemonic",
+  "pk-from-mnemonic",
+  "out-pk-from-mnemonic",
+  pkFromMnemonic
+);
+
+attachHandler(
+  "btn-pk-to-addresses",
+  "pk-to-addresses",
+  "out-pk-to-addresses",
+  pkToAddresses
+);
+
+const mnemonic = bip39.generateMnemonic();
+document.getElementById("pk-from-mnemonic").value = mnemonic;
