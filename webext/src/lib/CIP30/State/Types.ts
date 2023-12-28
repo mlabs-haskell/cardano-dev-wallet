a50a1d7a6826fe33600f1a53809212dd87bf8cc0
Farseen <oss@itsfarseen.anonaddy.me>
2023-12-28 17:41:40 +0530

    Implement state management for CIP30

---
+++ webext/src/lib/CIP30/State/Types.ts
@@ -0,0 +1,36 @@
+type Backend =
+  | {
+    type: "blockfrost";
+    name: string,
+    projectId: string;
+  }
+  | {
+    type: "ogmios_kupo";
+    name: string,
+    ogmiosUrl: string;
+    kupoUrl: string;
+  };
+
+interface RootKey {
+  name: string;
+  keyBech32: string;
+}
+
+interface Account {
+  name: string;
+  keyId: number;
+  accountIdx: number;
+}
+
+interface Overrides {
+  balance: number | null;
+  hiddenUtxos: Utxo[];
+  hiddenCollateral: Utxo[];
+}
+
+interface Utxo {
+  txHashHex: string;
+  idx: number;
+}
+
+export type { Backend, RootKey, Account, Overrides, Utxo }
